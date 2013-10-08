8970b01b57d8e6fb30db441a8afa90d69695a225
Anders Nylund <stokkheim@gmail.com>
2013-10-08 16:14:11 +0300

    merged develop

--- src/pellmonweb.py
+++ src/pellmonweb.py
@@ -173,7 +173,7 @@ class PellMonWebb:
 
         if(int(maxWidth) < 440):
             maxWidth = '440'
-
+        maxWidth = str(int(maxWidth) - 200)
         # The width passed to rrdtool does not include the sidebars
         graphWidth = str(maxWidth)
 
@@ -182,9 +182,9 @@ class PellMonWebb:
         graphTimeStart=str(graphTime + offset)
         graphTimeEnd=str(offset)
         #Build the command string to make a graph from the database
-        fd=NamedTemporaryFile(suffix='.png')
+        fd=NamedTemporaryFile(suffix='.svg')
         graph_file=fd.name
-        RrdGraphString1="rrdtool graph "+graph_file+" --lower-limit 0 --right-axis 1:0 --full-size-mode --width "+graphWidth+" --height 400 --end now-"+graphTimeEnd+"s --start now-"+graphTimeStart+"s "
+        RrdGraphString1="rrdtool graph "+graph_file+" --imgformat SVG --lower-limit 0 --right-axis 1:0 --full-size-mode --width "+graphWidth+" --height 400 --end now-"+graphTimeEnd+"s --start now-"+graphTimeStart+"s "
         RrdGraphString1=RrdGraphString1+"DEF:tickmark=%s:_logtick:AVERAGE TICK:tickmark#E7E7E7:1.0 "%db
         for key,value in polldata:
             if cherrypy.session.get(value)=='yes' and colorsDict.has_key(key):
@@ -193,7 +193,7 @@ class PellMonWebb:
 
         os.system(RrdGraphString1)
         cherrypy.response.headers['Pragma'] = 'no-cache'
-        return serve_fileobj(fd, content_type='image/png')
+        return serve_fileobj(fd, content_type='image/svg+xml')
 
     @cherrypy.expose
     def consumption(self, **args):
@@ -214,9 +214,9 @@ class PellMonWebb:
             # The width passed to rrdtool does not include the sidebars
             graphWidth = str(maxWidth)
 
-            fd=NamedTemporaryFile(suffix='.png')
+            fd=NamedTemporaryFile(suffix='.svg')
             consumption_file=fd.name
-            RrdGraphString1="rrdtool graph "+consumption_file+" --full-size-mode --width "+graphWidth+" --right-axis 1:0 --right-axis-format %%1.1lf --height 400 --end %u --start %u-86400s "%(now,now)
+            RrdGraphString1="rrdtool graph "+consumption_file+" --imgformat SVG --full-size-mode --width "+graphWidth+" --right-axis 1:0 --right-axis-format %%1.1lf --height 400 --end %u --start %u-86400s "%(now,now)
             RrdGraphString1=RrdGraphString1+"DEF:a=%s:feeder_time:AVERAGE DEF:b=%s:feeder_capacity:AVERAGE "%(db,db)
             for h in range(0,24):
                 start=(now-h*3600-3600)
@@ -227,7 +227,7 @@ class PellMonWebb:
             RrdGraphString1=RrdGraphString1+" >>/dev/null"
             os.system(RrdGraphString1)
             cherrypy.response.headers['Pragma'] = 'no-cache'
-            return serve_fileobj(fd, content_type='image/png')
+            return serve_fileobj(fd, content_type='image/svg+xml')
 
     @cherrypy.expose
     @require() #requires valid login
